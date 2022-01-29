aa54c0d0dcf5be0323d64a958c0b09d5511085c4
joanbelk <joanbelk2@gmail.com>
2022-01-28 22:27:29 -0400

    last page

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-document.query
+var randomNumber1 = Math.floor(Math.random()*6+1);
+var source = "images/dice" + randomNumber1 + ".png"
+document.querySelectorAll("img")[0].setAttribute("src",source);
+
+var randomNumber2 = Math.floor(Math.random()*6+1);
+var source2 = "images/dice" + randomNumber2 + ".png"
+document.querySelectorAll("img")[1].setAttribute("src",source2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Play 2 Wins! 🚩";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
